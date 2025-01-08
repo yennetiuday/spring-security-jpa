0c921f950fb5712ac0e25bbdf8c29f825acd8b9c
UDAYBHASKAR\yenne <yenneti.uday@hotmail.com>
2025-01-08 17:40:07 +0530

    Spring boot security project using JPA

---
+++ src/main/java/com/uday/spring_security_jpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package com.uday.spring_security_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+}
